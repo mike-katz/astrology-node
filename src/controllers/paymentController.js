df2039b77abb3ca70ddfbfbec512c6cc72160ddf
sksamip <ryan.lee36152@gmail.com>
2025-12-24 18:13:07 +0530

    add payment list api

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -15,13 +15,82 @@ async function addPayment(req, res) {
         await db('users').where({ id: user?.id }).increment({ balance: Number(amount) });
         await db('payments').insert({ user_id: req?.userId, transaction_id: orderId, utr, amount, status: "success", type: "recharge" });
         await db('balancelogs').insert({ user_id: req?.userId, message: "Purchase of AT-Money via razorpay", amount });
-        return res.status(200).json({ success: true, message: 'Review added Successfully' });
+        return res.status(200).json({ success: true, message: 'Payment added Successfully' });
     } catch (err) {
         console.error(err);
         res.status(500).json({ success: false, message: 'Server error' });
     }
 }
 
+async function getPayment(req, res) {
+    try {
+        let page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 100;
 
+        if (page < 1) page = 1;
+        if (limit < 1) limit = 100;
+        const offset = (page - 1) * limit;
+        const log = await db('payments')
+            .whereNull('deleted_at')
+            .andWhere('user_id', req.userId)
+            .orderBy('id', 'desc')
+            .limit(limit)
+            .offset(offset);
 
-module.exports = { addPayment };
+        const [{ count }] = await db('payments')
+            .count('* as count')
+            .whereNull('deleted_at')
+            .andWhere('user_id', req.userId);
+        const total = parseInt(count);
+        const totalPages = Math.ceil(total / limit);
+
+        const response = {
+            page,
+            limit,
+            total,
+            totalPages,
+            results: log
+        }
+        return res.status(200).json({ success: true, data: response, message: 'List Successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, message: 'Server error' });
+    }
+}
+
+async function getTransactions(req, res) {
+    try {
+        let page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 100;
+
+        if (page < 1) page = 1;
+        if (limit < 1) limit = 100;
+        const offset = (page - 1) * limit;
+        const log = await db('balancelogs')
+            .whereNull('deleted_at')
+            .andWhere('user_id', req.userId)
+            .orderBy('id', 'desc')
+            .limit(limit)
+            .offset(offset);
+
+        const [{ count }] = await db('balancelogs')
+            .count('* as count')
+            .whereNull('deleted_at')
+            .andWhere('user_id', req.userId);
+        const total = parseInt(count);
+        const totalPages = Math.ceil(total / limit);
+
+        const response = {
+            page,
+            limit,
+            total,
+            totalPages,
+            results: log
+        }
+        return res.status(200).json({ success: true, data: response, message: 'List Successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, message: 'Server error' });
+    }
+}
+module.exports = { addPayment, getPayment, getTransactions };
--- src/routes/payment.js
+++ src/routes/payment.js
@@ -3,4 +3,7 @@ const router = express.Router();
 const payment = require('../controllers/paymentController');
 
 router.post('/', payment.addPayment);
+router.get('/', payment.getPayment);
+router.get('/transactions', payment.getTransactions);
+
 module.exports = router;
