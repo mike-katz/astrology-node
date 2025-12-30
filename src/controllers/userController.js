23089e293b6b05bb6ba209ca53f8a7a4da218934
sksamip <ryan.lee36152@gmail.com>
2025-12-30 20:11:11 +0530

    add profile api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -109,7 +109,7 @@ async function updateToken(req, res) {
     const { token } = req.body;
     try {
         const order = await db('users').where({ id: req.userId }).first();
-        if (!order) return res.status(400).json({ success: false, message: 'Pandit not found.' });
+        if (!order) return res.status(400).json({ success: false, message: 'User not found.' });
         const update = {}
         if (token) {
             update.token = token
@@ -121,4 +121,28 @@ async function updateToken(req, res) {
         res.status(500).json({ success: false, message: 'Server error' });
     }
 }
-module.exports = { updateProfile, getProfile, getBalance, updateToken };
+
+async function profileUpdate(req, res) {
+    try {
+        const order = await db('users').where({ id: req.userId }).first();
+        if (!order) return res.status(400).json({ success: false, message: 'User not found.' });
+        const update = {}
+        const { files } = req
+        if (files?.profile?.length > 0) {
+            const image = await uploadImageTos3('profile', files?.profile[0], 'upload');
+            update.profile = image.data.Location;
+        }
+        if (order?.profile?.length > 0) {
+            const dd = await deleteFileFroms3(decodeURIComponent(order?.profile))
+            console.log("dd", dd);
+        }
+        await db('users').where({ id: req.userId }).update(update);
+        return res.status(200).json({ success: true, data: null, message: 'Update successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, message: 'Server error' });
+    }
+}
+
+
+module.exports = { updateProfile, getProfile, getBalance, updateToken, profileUpdate };
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,40 @@ const express = require('express');
 const router = express.Router();
 const user = require('../controllers/userController');
 
+const multer = require('multer');
+const storage = multer.memoryStorage(); // or diskStorage
+
+const upload = multer({
+    storage,
+    limits: {
+        files: 20,              // total files limit
+        fileSize: 5 * 1024 * 1024 // 5MB per file
+    },
+    fileFilter: (req, file, cb) => {
+        const allowedTypes = [
+            'image/jpeg',
+            'image/png',
+            'image/jpg',
+            'application/pdf'
+        ];
+
+        if (allowedTypes.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb(new Error('Only images & PDFs allowed'), false);
+        }
+    }
+});
+
 router.get('/', user.getProfile);
 router.get('/balance', user.getBalance);
 router.post('/update', user.updateProfile);
 router.post('/updateToken', user.updateToken);
+
+router.post('/profileUpdate',
+    upload.fields([
+        { name: 'profile', maxCount: 1 },
+    ]),
+    user.profileUpdate);
+
 module.exports = router;
