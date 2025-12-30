28a26434284094dfb319200fa7098a3a0f1fcef2
sksamip <ryan.lee36152@gmail.com>
2025-12-30 15:49:22 +0530

    change path

--- src/utils/generatepdf.js
+++ src/utils/generatepdf.js
@@ -15,7 +15,12 @@ async function generateInvoicePDF(data) {
 
     const browser = await puppeteer.launch({
         headless: 'new',
-        args: ["--no-sandbox", "--disable-setuid-sandbox"]
+        executablePath: '/snap/bin/chromium',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage'
+        ]
     });
     const page = await browser.newPage();
 
