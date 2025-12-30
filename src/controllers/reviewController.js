00a84221af365d0f3ecd9117fda5500b4fd46e78
sksamip <ryan.lee36152@gmail.com>
2025-12-30 16:11:22 +0530

    add review increase in pandit

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -14,6 +14,7 @@ async function addReview(req, res) {
         console.log("user", user);
         // if (user) return res.status(400).json({ success: false, message: 'You already follow this pandit' });
         if (!user) {
+            await db('pandits').where({ id: Number(panditId) }).increment(`rating_${rating}`, 1);
             await db('reviews').insert({ user_id: req?.userId, pandit_id: panditId, order_id: orderId, message, rating, type: "user" });
         } else {
             await db('reviews').where({ id: user?.id }).update({ user_id: req?.userId, pandit_id: panditId, order_id: orderId, message, rating });
