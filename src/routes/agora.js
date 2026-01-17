54a8eef23106dc594e5d15ae2456d142765203ed
sksamip <ryan.lee36152@gmail.com>
2026-01-17 13:19:28 +0530

    post methdd

--- src/routes/agora.js
+++ src/routes/agora.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const agora = require('../controllers/agoraController');
 
-router.get('/rtc-token', agora.getRtcToken);
+router.post('/rtc-token', agora.getRtcToken);
 // router.post('/token', agora.token);
 // router.post('/recording/start', agora.recordingStart);
 // router.post('/recording/stop', agora.recordingStop);
