b93b3cf3211d3589790afcc1cd5d3bc911861e1b
sksamip <ryan.lee36152@gmail.com>
2026-02-09 19:54:18 +0530

    id

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -298,7 +298,7 @@ async function getDetail(req, res) {
         if (!order && orderDetail?.pandit_id != null) {
             order = await db('pandits').where({ id: orderDetail?.pandit_id }).first();
         }
-        const response = { id: panditId, name: order?.display_name, status: order?.status, profile: order?.profile, isOnline: order?.chat, is_free: orderDetail?.is_free, pandit_id: order?.pandit_id }
+        const response = { id: panditId, name: order?.display_name, status: order?.status, profile: order?.profile, isOnline: order?.chat, is_free: orderDetail?.is_free, pandit_id: orderDetail?.pandit_id }
         if (orderDetail) {
             response.startTime = orderDetail?.start_time;
             response.endTime = orderDetail?.end_time;
