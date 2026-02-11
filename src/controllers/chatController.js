a66810f046730b2653bdee9f4b7705b7ea52eb19
sksamip <ryan.lee36152@gmail.com>
2026-02-12 02:35:42 +0530

    fix second count

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -455,10 +455,15 @@ async function endChat(req, res) {
         if (!order) {
             return res.status(400).json({ success: false, message: 'Wrong order. Please enter correct' });
         }
-        const diffMinutes = getDuration(order.start_time, new Date())
-        // console.log("endChat diffMinutes", diffMinutes, "startTime", order.start_time, "endTime", new Date());
+        // const diffMinutes = getDuration(order.start_time, new Date());
+        const diffMs = Math.abs(new Date() - new Date(order.start_time));
+        const totalSeconds = Math.floor(diffMs / 1000);
         const setting = await db('settings').first();
-        if ((diffMinutes < Number(setting?.chat_end_min_minutes)) && !order?.is_free) return res.status(400).json({ success: false, message: "Can't end chat in first 1 minute." });
+        console.log("totalSeconds", totalSeconds);
+        const minSec = setting?.chat_end_min_minutes * 60
+        console.log("minSec required", minSec);
+        // console.log("endChat diffMinutes", diffMinutes, "startTime", order.start_time, "endTime", new Date());
+        if ((totalSeconds < Number(minSec)) && !order?.is_free) return res.status(400).json({ success: false, message: "Can't end chat in first 1 minute." });
         // const [{ total }] = await db('orders').where({ pandit_id: order?.pandit_id, user_id: req.userId }).count('id as total');
         // if (total == 1) {
         //     return res.status(400).json({ success: false, message: 'You can not end this chat.' });
