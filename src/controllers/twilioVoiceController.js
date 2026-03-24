0e8cec7efc8d24f9637a35328e8156d9d95822b7
sksamip <ryan.lee36152@gmail.com>
2026-03-24 18:36:34 +0530

    recording upload in s3

--- src/controllers/twilioVoiceController.js
+++ src/controllers/twilioVoiceController.js
@@ -1,6 +1,9 @@
 const db = require('../db');
 require('dotenv').config();
 const twilio = require('twilio');
+const S3_BUCKET = process.env.S3_RECORDING_BUCKET || process.env.AWS_BUCKET_NAME;
+const S3_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+const S3_SECRET_KEY = process.env.AWS_SECRET_KEY;
 
 async function voice(req, res) {
     try {
@@ -27,7 +30,6 @@ async function voice(req, res) {
         res.send("something went to wrong");
     }
 }
-
 
 async function generateToken(req, res) {
     try {
@@ -70,6 +72,60 @@ async function callback(req, res) {
 async function recording(req, res) {
     console.log("recording query param", req.query);
     console.log("recording body param", req.body);
+
+    const { RecordingUrl, RecordingSid, CallSid } = req.body;
+
+    console.log("Recording URL:", RecordingUrl);
+
+    // 🔹 1. Download recording (mp3 format)
+
+
+    const response = await axios({
+        method: 'GET',
+        url: RecordingUrl + '.mp3',
+        responseType: 'arraybuffer',
+        auth: {
+            username: process.env.TWILIO_ACCOUNT_SID,
+            password: process.env.TWILIO_AUTH_TOKEN
+        }
+    });
+
+    const fileBuffer = Buffer.from(response.data, 'binary');
+
+    // 🔹 2. Upload to S3
+    const fileName = `recordings/${CallSid}_${Date.now()}.mp3`;
+
+    await s3.upload({
+        Bucket: S3_BUCKET,
+        Key: fileName,
+        Body: fileBuffer,
+        ContentType: 'audio/mpeg'
+    }).promise();
+
+    const s3Url = `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`;
+
+    console.log("S3 URL:", s3Url);
+
+    // 🔹 3. Save in DB (example Mongo)
+    await db.collection('call_recordings').insertOne({
+        callSid: CallSid,
+        recordingSid: RecordingSid,
+        recordingUrl: s3Url,
+        createdAt: new Date()
+    });
+
+    // 🔹 4. Delete from Twilio
+    const twilio = require('twilio')(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_AUTH_TOKEN
+    );
+
+    await twilio.recordings(RecordingSid).remove();
+
+    console.log("Deleted from Twilio");
+
+    res.sendStatus(200);
+
 }
 
 
