87dcb522f1846e4b70c109843e299b79c01ad3c3
sksamip <ryan.lee36152@gmail.com>
2026-06-20 12:56:55 +0530

    add log

--- src/controllers/callbackController.js
+++ src/controllers/callbackController.js
@@ -281,7 +281,8 @@ async function razorpay(req, res) {
 }
 
 async function xpay(req, res) {
-    logger.info('xpay callback start', req.body);
+    logger.info('xpay callback start body', req.body);
+    logger.info('xpay callback start query', req.query);
     const { eventType, intentId, status, receiptId } = req.body
     try {
         if (eventType != 'intent.success') {
