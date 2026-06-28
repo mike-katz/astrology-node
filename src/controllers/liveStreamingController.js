d6753bb97927323c0fc76ede6a3b47c7da777c75
sksamip <ryan.lee36152@gmail.com>
2026-06-28 15:52:04 +0530

    add success true

--- src/controllers/liveStreamingController.js
+++ src/controllers/liveStreamingController.js
@@ -819,7 +819,7 @@ function getDuration(start_time, end_time) {
 async function stopLive(req, res) {
     const { channel_id } = req.body
     try {
-        return res.status(200).json({ success: false, message: 'Live ended.' });
+        return res.status(200).json({ success: true, message: 'Live ended.' });
         const active = await db('live_streams').where({ channel_id, status: 'live' }).first();
         if (!active) {
             return res.status(200).json({ success: false, message: 'No active live to stop.' });
