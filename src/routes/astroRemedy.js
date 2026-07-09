dbbc1e8c6a179ba951207ef0fb4b792a4fdffa0c
sksamip <ryan.lee36152@gmail.com>
2026-07-10 01:07:55 +0530

    change route

--- src/routes/astroRemedy.js
+++ src/routes/astroRemedy.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const astroRemedy = require('../controllers/astroRemedyController');
 
 router.get('/', astroRemedy.getRemedyList);
-router.get('/pooja', astroRemedy.getRemedyItems);
+router.get('/product', astroRemedy.getRemedyItems);
 router.get('/detail', astroRemedy.getRemedyDetail);
 
 module.exports = router;
