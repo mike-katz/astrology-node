069ed7e6296f0da9cc48fe651f9a40383ce9c3ff
sksamip <ryan.lee36152@gmail.com>
2026-07-11 14:56:37 +0530

    add order list api

--- src/routes/astroRemedy.js
+++ src/routes/astroRemedy.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const astroRemedy = require('../controllers/astroRemedyController');
+const remedyOrder = require('../controllers/remedyOrderController');
 
 router.get('/', astroRemedy.getRemedyList);
 router.get('/product', astroRemedy.getRemedyItems);
 router.get('/detail', astroRemedy.getRemedyDetail);
 router.post('/order/create', astroRemedy.getRemedyOrderCreate);
+router.get('/order/list', remedyOrder.getUserOrders);
 
 module.exports = router;
