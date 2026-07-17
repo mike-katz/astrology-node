0839a76aec8a7ee0651d81096c3499979637818a
sksamip <ryan.lee36152@gmail.com>
2026-07-17 22:22:03 +0530

    add user offer 11 rs change

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -516,14 +516,18 @@ async function balanceCut(user_id, order, end_time, place) {
             if (newBalance < 0) throw new Error('INSUFFICIENT_BALANCE');
 
             // 5. Key guard — only 1 request pass thase
+
+            const updOrder = {
+                status: 'completed',
+                duration: diffMinutes,
+                end_time: new Date(end_time),
+            }
+            if (!isFreeOrder) {
+                updOrder.deduction = deduction
+            }
             const rowsUpdated = await trx('orders')
                 .where({ id: lockedOrder.id, status: 'continue' })
-                .update({
-                    status: 'completed',
-                    deduction,
-                    duration: diffMinutes,
-                    end_time: new Date(end_time),
-                });
+                .update(updOrder);
 
             // 6. rowsUpdated = 0 → second request → skip
             if (!rowsUpdated) {
@@ -1024,9 +1028,9 @@ async function newCreateOrder(req, res) {
             // const currencyItem = JSON.parse(settings?.currency_amount || '[]').find(
             //     item => item?.currency === (user?.default_currency || 'INR')
             // );
-
+            const payment = await db('payments').where({ user_id: req.userId, status: "success" }).whereNot('offer_amount', 0).first();
             duration = Math.floor(Number(Number(user?.offer_amount)) / Number(panditRate));
-            deduction = Number(user?.offer_amount)
+            deduction = Number(payment?.amount)
 
             // if ((user?.balance) < currencyItem?.amount) {
             //     logger.info('order_create fail', { userId: req.userId, message: 'Please recharge your wallet.' });
--- src/controllers/liveStreamingController.js
+++ src/controllers/liveStreamingController.js
@@ -619,8 +619,9 @@ async function createMediaOrder(req, res) {
             //     return res.status(400).json({ success: false, message: 'Please recharge your wallet.' });
             // }
             // deduction = currencyItem?.amount
+            const payment = await db('payments').where({ user_id: req.userId, status: "success" }).whereNot('offer_amount', 0).first();
             duration = Math.floor(Number(Number(user?.offer_amount)) / Number(panditRate));
-            deduction = Number(user?.offer_amount)
+            deduction = Number(payment?.amount)
         }
 
         const orderId = `${new Date().getTime().toString()}${Math.floor(100000 + Math.random() * 900000).toString()}`;
