723428dbfe9ac6202ad4815eadc86921644f7a8c
sksamip <ryan.lee36152@gmail.com>
2026-08-03 17:37:19 +0530

    add faq api change

--- src/controllers/astroRemedyController.js
+++ src/controllers/astroRemedyController.js
@@ -341,8 +341,8 @@ async function getRemedyFaq(req, res) {
             return res.status(400).json({ success: false, message: 'Item id is required.' });
         }
 
-        const reviews = await db('remedy_faqs')
-            .where({ type })
+        const reviews = await db('faqs')
+            .where({ type: "pooja" })
             .orderBy('id', 'desc');
         return res.status(200).json({
             success: true,
