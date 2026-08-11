cf8d5536f2a015953a0259ea44b445dd65ec63c3
sksamip <ryan.lee36152@gmail.com>
2026-08-11 22:32:06 +0530

    add routee

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
-const { register, login, verifyOtp, socialUrl, getSettings, sendCall, test, googleLogin, appleLogin } = require('../controllers/authController');
+const { register, login, verifyOtp, socialUrl, getSettings, sendCall, test, googleLogin, appleLogin, getCountryByIp } = require('../controllers/authController');
 
 router.get('/setting', getSettings);
 
